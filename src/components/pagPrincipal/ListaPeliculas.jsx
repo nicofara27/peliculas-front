f59e7705cc084223dc07dddbf1d4e40dfcbe18c4
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-04-17 11:32:18 -0300

    Cambios en el espacio entre cada card

--- src/components/pagPrincipal/ListaPeliculas.jsx
+++ src/components/pagPrincipal/ListaPeliculas.jsx
@@ -5,7 +5,7 @@ import CardPelicula from "./CardPelicula";
 const ListaPeliculas = ({peliculas}) => {  
   return (
     <section id="peliculas">
-      <Row justify="space-between"  gutter={[8, 40]}>
+      <Row justify="space-between"  gutter={[0, 32]}>
       {peliculas.map((pelicula)=><CardPelicula key={pelicula.id} pelicula={pelicula}></CardPelicula>)}    
       </Row>
     </section>
