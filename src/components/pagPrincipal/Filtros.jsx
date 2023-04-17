f16e51db5bd9f35051c074b53aaba79bec13f7b7
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-04-17 11:26:18 -0300

    Agregue el buscador

--- src/components/pagPrincipal/Filtros.jsx
+++ src/components/pagPrincipal/Filtros.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import { Col, Input, Row } from "antd";
+import React from "react";
 
 const Filtros = () => {
-    return (
-        <section>
-            <button className='filtrosBtn'>Popular</button>
-            <button className='filtrosBtn'>Nuevas</button>
-            <button className='filtrosBtn'>Mejores votadas</button>
-            <button className='filtrosBtn'>Proximas</button>
-        </section>
-    );
+    const onSearch = (value) => console.log(value);
+  return (
+    <section>
+      <Row justify={"space-between"}>
+        <Col>
+          <button className="filtrosBtn">Popular</button>
+          <button className="filtrosBtn">Nuevas</button>
+          <button className="filtrosBtn">Mejores votadas</button>
+          <button className="filtrosBtn">Proximas</button>
+        </Col>
+        <Col>
+          <Input.Search
+            placeholder="Buscar por nombre de pelicula"
+            allowClear
+            onSearch={onSearch}
+            style={{
+              width: 300,
+              height: "100%",
+            }}
+          />
+        </Col>
+      </Row>
+    </section>
+  );
 };
 
 export default Filtros;
