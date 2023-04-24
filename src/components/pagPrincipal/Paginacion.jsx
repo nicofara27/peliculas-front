dad140dcf2f2c136a28f18a783db146cb23bc74f
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-04-24 20:47:58 -0300

    Agregue funcion para cambiar la url cuando se cambia de pagina

--- src/components/pagPrincipal/Paginacion.jsx
+++ src/components/pagPrincipal/Paginacion.jsx
@@ -1,14 +1,27 @@
 import { Pagination } from "antd";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Paginacion = ({ pagina, setPagina }) => {
+  const navigate = useNavigate();
+
+  // Funcion para cambiar de pagina
+  const cambiarPagina = (pag) => {
+    setPagina(pag);
+    navigate(`/pagina/${pag}`);
+  };
+
+  // Condicional que cambia a la pagina numero 1 de la lista de peliculas cuando se navega hacia la pagina principal
+  if (window.location.href === "http://localhost:3000/") {
+    setPagina(1);
+  }
   return (
     <section id="paginacion">
       <Pagination
         total={200}
         current={pagina}
         showSizeChanger={false}
-        onChange={(pag) => setPagina(pag)}
+        onChange={cambiarPagina}
       />
     </section>
   );
