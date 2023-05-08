29337e6fbdd01e94c4f34b012432ee8e173a23ec
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-05-08 12:44:15 -0300

    Agregue la ruta para la pagina de ingreso y cambie unos estilos predefinidos de ant design

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,33 @@ import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import PagPelicula from "./components/view/PagPelicula";
 import PagPrincipal from "./components/view/PagPrincipal";
+import PagIngreso from "./components/view/PagIngreso";
+import { ConfigProvider } from "antd";
 
 function App() {
 
   return (
     <BrowserRouter>
+    <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary:"#13def0",
+        colorText:"#ddd",
+        colorTitle:"#ddd",
+      }
+    }}
+    >
       <Header></Header>
       <Routes>
         <Route exact path="/" element={<PagPrincipal />}></Route>
         <Route exact path="/pagina/:numero" element={<PagPrincipal />}></Route>
         <Route exact path="/buscar/:nombre/:numero" element={<PagPrincipal />}></Route>
+        <Route exact path="/ingresar" element={<PagIngreso />}></Route>
+        <Route exact path="/registro" element={<PagIngreso />}></Route>
         <Route exact path="/:id" element={<PagPelicula />}></Route>
       </Routes>
       <Footer></Footer>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
