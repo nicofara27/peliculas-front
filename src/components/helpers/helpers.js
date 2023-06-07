aa2291d71e6d3530a2a39e76bb02938c92152744
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-06-07 11:39:42 -0300

    Agregue funciones para agregar, listar y eliminar peliculas de la lista de cada usuario

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,38 +1,83 @@
-const URL = process.env.REACT_APP_API_USUARIOS
+const URL = process.env.REACT_APP_API_USUARIOS;
 
-export const crearUsuario = async(usuario) => {
-    try {
-        const respuesta = await fetch(URL + "nuevo", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(usuario)
-        });
-        return respuesta;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
-}
+export const crearUsuario = async (usuario) => {
+  try {
+    const respuesta = await fetch(URL + "nuevo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(usuario),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
 
-export const login = async(usuario) => {
-    try {
-        const respuesta = await fetch(URL, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(usuario)
-        });
-        const datos =await respuesta.json();
-        return {
-            status: respuesta.status,
-            mensaje: datos.mensaje,
-            nombre: datos.nombre,
-            token: datos.token,
-            uid: datos.uid,
-          };
-    } catch (error) {
-        console.log(error)
-        return false;
-    }
-}
+export const login = async (usuario) => {
+  try {
+    const respuesta = await fetch(URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(usuario),
+    });
+    const datos = await respuesta.json();
+    return {
+      status: respuesta.status,
+      mensaje: datos.mensaje,
+      nombre: datos.nombre,
+      token: datos.token,
+      uid: datos.uid,
+    };
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const listarPeliculas = async (nombreUsuario) => {
+  try {
+    const respuesta = await fetch(URL + nombreUsuario);
+    const listaPeliculas = await respuesta.json();
+    return listaPeliculas[0].lista;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const agregarALista = async (nombreUsuario, pelicula) => {
+  try {
+    const respuesta = await fetch(URL + nombreUsuario, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(pelicula),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const eliminarDeLista = async (nombreUsuario, peliculaAEliminar) => {
+  try {
+    const key = peliculaAEliminar.key.toString();
+    console.log(peliculaAEliminar);
+    const respuesta = await fetch(URL + nombreUsuario + "/" + key, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(peliculaAEliminar),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
