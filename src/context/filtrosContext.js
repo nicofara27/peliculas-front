30d23d70c9c0a9749be9f9e255de974ea98e1e73
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-05 12:16:34 -0300

    Agregue context para todo lo relacionado con filtros

---
+++ src/context/filtrosContext.js
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+export const FiltrosContext = createContext();
+
+export const FiltrosContextProvider = ({ children }) => {
+  const [pagina, setPagina] = useState(1);
+  const [filtro, setFiltro] = useState("popular");
+  const [buscar, setBuscar] = useState("");
+  const [categoria, setCategoria] = useState("");
+
+  return (
+    <FiltrosContext.Provider
+      value={{
+        pagina,
+        setPagina,
+        filtro,
+        setFiltro,
+        buscar,
+        setBuscar,
+        categoria,
+        setCategoria,
+      }}
+    >
+      {children}
+    </FiltrosContext.Provider>
+  );
+};
