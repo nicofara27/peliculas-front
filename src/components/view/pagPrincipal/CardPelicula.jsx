31f8e6119142a23145f658d8e20e3986cf8ed545
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-05 12:30:45 -0300

    Agregue link a cada categoria, recorte el titulo en caso de que sea muy largo y reduje el nro de categorias que se muestran

--- src/components/view/pagPrincipal/CardPelicula.jsx
+++ src/components/view/pagPrincipal/CardPelicula.jsx
@@ -1,12 +1,15 @@
 import { Col } from "antd";
 import { StarFilled } from "@ant-design/icons";
-import React from "react";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FiltrosContext } from "../../../context/filtrosContext";
 
 const CardPelicula = ({ pelicula }) => {
+  const { setCategoria } = useContext(FiltrosContext);
   const { title, vote_average, poster_path, genre_ids, id } = {
     ...pelicula,
   };
+  const navigate = useNavigate();
 
   const imagen = `https://www.themoviedb.org/t/p/w440_and_h660_face${poster_path}`;
   // Redondea el valor de la puntuacion
@@ -16,7 +19,7 @@ const CardPelicula = ({ pelicula }) => {
   }
 
   // Comprueba el codigo que viene de la api con la listaGeneros para definir a que generos pertenece la pelicula
-  let generosPelicula = "";
+  let generosPelicula = [];
   const listaGeneros = [
     {
       id: 28,
@@ -99,12 +102,22 @@ const CardPelicula = ({ pelicula }) => {
     let generoEncontrado = listaGeneros.find(
       (genero) => genero.id === idGenero
     );
-    if (generosPelicula === "") {
-      generosPelicula += generoEncontrado.name;
-    } else {
-      generosPelicula += ", " + generoEncontrado.name;
+    if (generosPelicula.length < 4) {
+      generosPelicula.push({
+        nombre: generoEncontrado.name,
+        id: generoEncontrado.id,
+      });
     }
   });
+
+  const irACategoria = (cat) => {
+    setCategoria(cat);
+    navigate(`/categoria/${cat}/1`);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <Col sm={12} md={11} lg={5} className="cardPelicula">
@@ -112,15 +125,25 @@ const CardPelicula = ({ pelicula }) => {
         <div>
           <img className="peliculasImg" src={imagen} alt={title} />
         </div>
-        <div className="peliculasDesc">
-          <div className="peliculasDesc__calificacion">
-            <StarFilled className="peliculasDesc__icon" />
-            <p>{puntuacion}</p>
-          </div>
-          <h2>{title.length>37?(title.slice(0,-20)+ "..."):(title)}</h2>
-          <p>{generosPelicula}</p>
-        </div>
       </Link>
+      <div className="peliculasDesc">
+        <div className="peliculasDesc__calificacion">
+          <StarFilled className="peliculasDesc__icon" />
+          <p>{puntuacion}</p>
+        </div>
+        <Link to={`/${id}`} className="peliculasDesc__titulo">
+          {title.length > 32 ? title.slice(0, -15) + "..." : title}
+        </Link>
+        <ul className="peliculasDesc__categoria">
+          {generosPelicula.map((genero) => (
+            <li key={genero.id}>
+              <button onClick={() => irACategoria(genero.id)}>
+                {genero.nombre}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Col>
   );
 };
