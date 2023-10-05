4e8a460286ffbfe0d49f0e1ecde2b63dabcb3a4c
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-05 19:13:57 -0300

    Pase la lista de generos a un archivo aparte

--- src/pages/pagPrincipal/components/CardPelicula.jsx
+++ src/pages/pagPrincipal/components/CardPelicula.jsx
@@ -3,6 +3,7 @@ import { StarFilled } from "@ant-design/icons";
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiltrosContext } from "../../../context/filtrosContext";
+import { listaGeneros } from "../../../constants";
 
 const CardPelicula = ({ pelicula }) => {
   const { setCategoria } = useContext(FiltrosContext);
@@ -19,85 +20,7 @@ const CardPelicula = ({ pelicula }) => {
   }
 
   // Comprueba el codigo que viene de la api con la listaGeneros para definir a que generos pertenece la pelicula
-  let generosPelicula = [];
-  const listaGeneros = [
-    {
-      id: 28,
-      name: "Action",
-    },
-    {
-      id: 12,
-      name: "Adventure",
-    },
-    {
-      id: 16,
-      name: "Animation",
-    },
-    {
-      id: 35,
-      name: "Comedy",
-    },
-    {
-      id: 80,
-      name: "Crime",
-    },
-    {
-      id: 99,
-      name: "Documentary",
-    },
-    {
-      id: 18,
-      name: "Drama",
-    },
-    {
-      id: 10751,
-      name: "Family",
-    },
-    {
-      id: 14,
-      name: "Fantasy",
-    },
-    {
-      id: 36,
-      name: "History",
-    },
-    {
-      id: 27,
-      name: "Horror",
-    },
-    {
-      id: 10402,
-      name: "Music",
-    },
-    {
-      id: 9648,
-      name: "Mystery",
-    },
-    {
-      id: 10749,
-      name: "Romance",
-    },
-    {
-      id: 878,
-      name: "Science Fiction",
-    },
-    {
-      id: 10770,
-      name: "TV Movie",
-    },
-    {
-      id: 53,
-      name: "Thriller",
-    },
-    {
-      id: 10752,
-      name: "War",
-    },
-    {
-      id: 37,
-      name: "Western",
-    },
-  ];
+  let generosPelicula = []; 
   genre_ids.forEach((idGenero) => {
     let generoEncontrado = listaGeneros.find(
       (genero) => genero.id === idGenero
