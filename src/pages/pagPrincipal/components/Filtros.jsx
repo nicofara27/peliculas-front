40fa2a118f435cd2303289feb04c29acbfed9bde
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-05 19:15:10 -0300

    Pase el condicional que muestra el titulo con la busqueda o categoria o los filtros y el buscador

--- src/pages/pagPrincipal/components/Filtros.jsx
+++ src/pages/pagPrincipal/components/Filtros.jsx
@@ -1,19 +1,12 @@
 import { useContext } from "react";
 import { FiltrosContext } from "../../../context/filtrosContext";
-import { Col, Input, Radio, Row } from "antd";
+import { Col, Input, Row } from "antd";
 import { useNavigate } from "react-router-dom";
+import FiltrosOTitulo from "./FiltrosOTitulo";
 
 const Filtros = () => {
-  const {
-    filtro,
-    setFiltro,
-    setBuscar,
-    buscar,
-    categoria,
-    setCategoria,
-    setPagina,
-    pagina,
-  } = useContext(FiltrosContext);
+  const { filtro, setFiltro, setBuscar, setCategoria, setPagina, pagina } =
+    useContext(FiltrosContext);
   const navigate = useNavigate();
 
   // Funcion para cambiar los filtros de la lista de peliculas e ir a la primera pagina de la lista
@@ -37,59 +30,10 @@ const Filtros = () => {
     }
   };
 
-  // Condicional que maqueta la lista de flitros o el nombre de la pelicula buscada
-  const condicionalFiltroOBuscar = 
-    buscar === "" ? (<Col>
-      <h1 id="filtrosTitulo">Resultados para: {categoria}</h1>
-    </Col>): (
-      <Col>
-      <h1 id="filtrosTitulo">Resultados para: {buscar}</h1>
-    </Col>)
-    
-    console.log(categoria)
-    
-  const componenteCondicional =
-    buscar === "" && categoria === "" ? (
-      <Col>
-        <Radio.Group defaultValue="a" buttonStyle="solid">
-          <Radio.Button
-            value="a"
-            id="popular"
-            onClick={(e) => cambiarFiltro(e)}
-          >
-            Popular
-          </Radio.Button>
-          <Radio.Button
-            value="b"
-            id="now_playing"
-            onClick={(e) => cambiarFiltro(e)}
-          >
-            Ahora en cines
-          </Radio.Button>
-          <Radio.Button
-            value="c"
-            id="top_rated"
-            onClick={(e) => cambiarFiltro(e)}
-          >
-            Mejores votadas
-          </Radio.Button>
-          <Radio.Button
-            value="d"
-            id="upcoming"
-            onClick={(e) => cambiarFiltro(e)}
-          >
-            Proximas
-          </Radio.Button>
-        </Radio.Group>
-      </Col>
-    ) : (
-      condicionalFiltroOBuscar
-    );
-
   return (
     <section>
       <Row justify={"space-between"}>
-        {componenteCondicional}
+        <FiltrosOTitulo cambiarFiltro={cambiarFiltro} />
         <Col xs={24} lg={8} id="buscadorContainer">
           <Input.Search
             placeholder="Buscar por nombre de pelicula"
---
+++ src/pages/pagPrincipal/components/FiltrosOTitulo.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import { FiltrosContext } from "../../../context/filtrosContext";
+import { Col, Radio } from "antd";
+
+const FiltrosOTitulo = ({cambiarFiltro}) => {
+  const { buscar, categoria } = useContext(FiltrosContext);
+  
+  // Condicional que maqueta la lista de flitros o el nombre de la pelicula buscada
+  const condicionalFiltroOBuscar =
+    buscar === "" ? (
+      <Col>
+        <h1 id="filtrosTitulo">Resultados para: {categoria}</h1>
+      </Col>
+    ) : (
+      <Col>
+        <h1 id="filtrosTitulo">Resultados para: {buscar}</h1>
+      </Col>
+    );
+
+  return (
+    <>
+      {buscar === "" && categoria === "" ? (
+        <Col>
+          <Radio.Group defaultValue="a" buttonStyle="solid">
+            <Radio.Button
+              value="a"
+              id="popular"
+              onClick={(e) => cambiarFiltro(e)}
+            >
+              Popular
+            </Radio.Button>
+            <Radio.Button
+              value="b"
+              id="now_playing"
+              onClick={(e) => cambiarFiltro(e)}
+            >
+              Ahora en cines
+            </Radio.Button>
+            <Radio.Button
+              value="c"
+              id="top_rated"
+              onClick={(e) => cambiarFiltro(e)}
+            >
+              Mejores votadas
+            </Radio.Button>
+            <Radio.Button
+              value="d"
+              id="upcoming"
+              onClick={(e) => cambiarFiltro(e)}
+            >
+              Proximas
+            </Radio.Button>
+          </Radio.Group>
+        </Col>
+      ) : (
+        condicionalFiltroOBuscar
+      )}
+    </>
+  );
+};
+
+export default FiltrosOTitulo;
