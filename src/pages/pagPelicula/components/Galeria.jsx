4fd8fefed36ae2480a61030d3db8a0ae08049673
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-17 18:02:48 -0300

    Elimine state y useEffect en desuso

--- src/pages/pagPelicula/components/Galeria.jsx
+++ src/pages/pagPelicula/components/Galeria.jsx
@@ -1,13 +1,6 @@
 import ImageGallery from "react-image-gallery";
-import React, { useEffect, useState } from "react";
 
 const Galeria = ({ imagenes }) => {
-  const [listaImagenes, setListaImagenes] = useState([]);
-
-  useEffect(() => {
-    setListaImagenes(imagenes);
-  }, [imagenes]);
-
   return (
     <section id="seccionGaleria">
       <h2>Galeria</h2>
@@ -17,7 +10,7 @@ const Galeria = ({ imagenes }) => {
         showFullscreenButton={false}
         showIndex={true}
         slideInterval={2000}
-        items={listaImagenes}
+        items={imagenes}
       />
     </section>
   );
